965fde65c05d396127ba31c16d952f0948ee3f40
samir176520 <spe176520@gmail.com>
2025-03-19 17:35:35 +0200

    add q2 in lab2

--- lab2/convertor/index.ts
+++ lab2/convertor/index.ts
@@ -5,3 +5,20 @@ It should provide options to convert the temperature to Fahrenheit, Kelvin, or b
 The program should display the converted temperature(s) according to the user choice.
 */
 
+let choice: any; // choice may be NULL
+choice = "both" // accept Fahrenheit(h), Kalvin(k), both
+let degree : number = 1 
+const fahr = (degree:number) :number => degree * (9/5) + 30
+const kalvin = (degree:number) : number => degree + 274.15
+
+switch (choice)
+{
+    case "f" : console.log(degree + "C in Fahrenheit = " + fahr(degree) + "F")
+    break
+    case "k" : console.log(degree + "C in Kalvin = " + kalvin(degree) + "k")
+    break
+    case "both" : console.log(degree + "C in fahrenheit = " + fahr(degree) + "F")
+    console.log(degree + "C in Kalvin = " + kalvin(degree) + "k")
+    break
+    default: console.log("Worng choice try again with Fahrenheit(h), Kalvin(k), both!")
+}
