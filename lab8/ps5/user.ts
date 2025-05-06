56c4679d22d8202debf9c267cf11de71f182c404
Samir Ahmed <samir176520@gmail.com>
2025-05-06 23:45:48 +0300

    solution q5 in lab8

---
+++ lab8/ps5/user.ts
@@ -0,0 +1,12 @@
+export class user 
+{
+    private firstName : string 
+    private lastName : string 
+    private age : number 
+
+    constructor(firstName: string, lastName: string, age : number) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+    }
+}
