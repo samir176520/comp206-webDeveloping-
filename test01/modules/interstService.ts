e4d341850322d7740205b12b3bba230ca9349706
Samir Ahmed <samir176520@gmail.com>
2025-05-09 20:17:43 +0300

    remove files

--- test01/modules/interstService.ts
+++
@@ -1,8 +0,0 @@
-import { Interest } from "../types";
-
-const interests: Interest[] = [];
-
-export const addInterest = (interest: Interest) => interests.push(interest);
-
-export const getInterestsByUserId = (userId: number) =>
-  interests.filter((i) => i.userId === userId);
